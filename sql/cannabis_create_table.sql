95be0c9f0cb38c8937d71474396f354229b1335d
kotkovnn <48518223+kotkovnn@users.noreply.github.com>
2023-03-15 15:52:58 +0400

    Create cannabis_create_table.sql

---
+++ sql/cannabis_create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS cannabis (
+    id int
+    ,uid uuid
+    ,strain varchar(200)
+    ,cannabinoid_abbreviation varchar(200)
+    ,cannabinoid varchar(200)
+    ,terpene varchar(200)
+    ,medical_use varchar(200)
+    ,health_benefit varchar(200)
+    ,category varchar(200)
+    ,type varchar(200)
+    ,buzzword varchar(200)
+    ,brand varchar(200)
+);
